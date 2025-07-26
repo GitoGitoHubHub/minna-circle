f692d544c29c5471aca44844fce08907e3f3c6a8
NOsub-bit <genteishiyou@yahoo.co.jp>
2025-07-26 22:36:22 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// ✅ Astroの設定
 export default defineConfig({
   integrations: [tailwind()],
-  // 出力方法の明示（ないと Vercel が迷うことがあります）
-  output: "server", // または "static"（どちらか明示）
+  output: "static", // ← ここを static に変更
 });
