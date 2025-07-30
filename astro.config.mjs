1369d73d93a50cf5c1d2d9c9b2b779a0d20e487d
NOsub-bit <genteishiyou@yahoo.co.jp>
2025-07-31 03:47:26 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static', // ←ここが超重要
-});
-export default defineConfig({
-  base: '/', // ←★追記！
+  base: '/', // ★必要ならここを残す
   integrations: [tailwind()],
   output: 'static',
 });
