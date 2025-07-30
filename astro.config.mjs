68765b1f04e5f024529f1551bea781936cca5333
NOsub-bit <genteishiyou@yahoo.co.jp>
2025-07-31 03:57:23 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/', // ★必要ならここを残す
+  base: '/', // ★必要に応じて変更（例：'/minna-circle/'）
   integrations: [tailwind()],
   output: 'static',
 });
