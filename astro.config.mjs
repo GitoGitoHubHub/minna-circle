f854f2a80baf3dee7489baefe19472ec480e2d36
NOsub-bit <genteishiyou@yahoo.co.jp>
2025-08-09 00:10:31 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel'; // または '@astrojs/vercel/static'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless"; // ←これ必須
 
 export default defineConfig({
+  output: "server",                   // ←これ必須
+  adapter: vercel(),                  // ←これ必須
   integrations: [tailwind()],
-  adapter: vercel(), // サーバーレス用、static 用いずれかを選択
-  // output は指定しない（アダプターに任せる）
 });
