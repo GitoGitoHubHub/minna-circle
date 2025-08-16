347e7e60109638d773704eb049cc6780a5e97b28
NOsub-bit <genteishiyou@yahoo.co.jp>
2025-08-17 00:32:09 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel"; // ← serverless ではなくこちら
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   output: "server",
