7bf3dec0842b7694d35896c4d12c05ad270595ce
NOsub-bit <genteishiyou@yahoo.co.jp>
2025-08-17 09:28:31 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'; // or '@astrojs/vercel/edge'
 
-// 本番URLをVercelの環境変数 SITE で入れる（未設定時はデプロイURLに合わせる）
 const SITE = process.env.SITE || 'https://minnacircle.vercel.app';
 
 export default defineConfig({
   site: SITE,
-  output: 'static',   // ★ これで静的固定
-  // integrations: [] // SSR系のインテグレーションは入れない
+  output: 'server',  // ★ SSR
+  adapter: vercel(), // ★ Vercelアダプタ
 });
